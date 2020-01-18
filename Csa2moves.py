8f7dd646cc2345097bd19fcb9b4458b49a74c4d6
ry <sgmrrc@gmail.com>
2020-01-18 15:39:42 +0900

    fix: ウォーズの観戦モード新形式に対応

--- Csa2moves.py
+++ Csa2moves.py
@@ -52,19 +52,46 @@ def moves2usi(pieces):
 	return [usis, sfens]
 
 
-def csa2moves(kif):
-	# ウォーズの棋譜 to 指し手の配列
-	moves = kif.split('"')[1]
-	moves = moves.split('\t')
-	moves.pop(-1)
+# ウォーズの棋譜 to 指し手の配列
+def kif2moves(kif):
+	moves = kif.split('[')[1]
+	moves = moves.replace('&quot;', '"')
+	moves = moves.split(']')[0]
+	moves = moves.split('{')
+	moves.pop(0)
+	# moves = kif.split('"')[1]
+	# moves = moves.split('\t')
+	# moves.pop(-1)
 
-	# 移動元/移動先/駒、時間にバラす
+	return moves
+
+
+# 移動元/移動先/駒、時間にバラす
+# "t":600,"n":0,"m":"+7968GI"},
+# +7968GI,L600	-3142GI,L599	SENTE_WIN_TORYO
+def moves2piecestimes(moves):
 	pieces = []
 	times = []
 	for move in moves:
-		pieces.append([move[1:3], move[3:5], move[5:7]])
-		times.append(int(move.split(",")[1][1:]))
+		# 指し手以外の文字列を回避
+		if '"m"' not in move:
+			continue
+		# 移動元/移動先/駒
+		piece = move.split('"m":')[1]
+		pieces.append([piece[2:4], piece[4:6], piece[6:8]])
+		#pieces.append([move[1:3], move[3:5], move[5:7]])
+		# 時間
+		time = move.split(',')[0].split(':')[1]
+		times.append(int(time))
+	return pieces, times
 
+
+
+def csa2moves(kif):
+	# ウォーズの棋譜 to 指し手の配列
+	moves = kif2moves(kif)
+	# 移動元/移動先/駒、時間にバラす
+	pieces, times = moves2piecestimes(moves)
 	# 指し手 to usiのmoves
 	usi = moves2usi(pieces)
 
